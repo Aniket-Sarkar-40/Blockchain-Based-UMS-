9a1578982f8f2419d26079f69e395c503a80893f
AniketSarkar40 <aniket10ce@gmail.com>
2024-03-20 12:39:25 +0530

    Mongo Mapping for private to public blockchain done. phase 2 completed

--- Private Chain/Main.py
+++ Private Chain/Main.py
@@ -2,93 +2,85 @@ from flask import Flask, request
 import flask
 import json
 from Private_Blockchain import PrivateBlockchain
+from flask_pymongo import PyMongo
+import sys
+
 app = Flask(__name__)
 import requests
 
 
 blockchain = PrivateBlockchain()
 
-mapping = {
-    "publicChainNode" : 5001,
-}
+
+app.config["MONGO_URI"] = (
+    "mongodb+srv://aniket15970:3Zmg1JzbEZWO3Gjw@esm.nrjexdy.mongodb.net/ums?retryWrites=true&w=majority"
+)
+mongo = PyMongo(app)
+db = mongo.db
+
 
 @app.route("/")
 def home():
     show_blocks = blockchain.show_blocks()
     blocks_dict = [block.to_dict() for block in show_blocks]
-    return flask.jsonify({
-        "message": "Blockchain with PoA",
-        "Blockchain": blocks_dict
-    })
+    return flask.jsonify({"message": "Blockchain with PoA", "Blockchain": blocks_dict})
+
 
 # validators get permission to join the network from project Leadership team
 # by submitting required documents.
 
 
-@app.route("/register/validator", methods=['POST'])
+@app.route("/register/validator", methods=["POST"])
 def register_validator():
     # payload should have id key
     payload = request.get_json(force=True)
-    if not payload['id']:
-        return flask.jsonify({
-            "message": "validator id is missing ...."
-        })
+    if not payload["id"]:
+        return flask.jsonify({"message": "validator id is missing ...."})
     blockchain.add_validator(payload)
-    return flask.jsonify({
-        "message": "validator added successfully.."
-    })
+    return flask.jsonify({"message": "validator added successfully.."})
 
 
 @app.route("/register/peer", methods=["POST"])
 def register_peer():
     # payload should contain public_key and amount
     payload = request.get_json(force=True)
-    if not payload['public_key']:
-        return flask.jsonify({
-            "message": "peer public key is missing ...."
-        })
+    if not payload["public_key"]:
+        return flask.jsonify({"message": "peer public key is missing ...."})
     blockchain.add_peer(payload)
-    return flask.jsonify({
-        "message": "peer added successfully.."
-    })
+    return flask.jsonify({"message": "peer added successfully.."})
 
 
 @app.route("/view_all_peers")
 def view_all_peers():
     peers = blockchain.view_all_peers()
-    return flask.jsonify({
-        "peers": peers
-    })
+    return flask.jsonify({"peers": peers})
 
 
 @app.route("/view_all_validators")
 def view_all_validators():
     validators = blockchain.view_all_validators()
-    return flask.jsonify({
-        "validators": validators
-    })
+    return flask.jsonify({"validators": validators})
 
 
 @app.route("/view_transaction_pool")
 def view_transaction_pool():
     transaction_pool = blockchain.view_all_transactions()
-    return flask.jsonify({
-        "transaction_pool": transaction_pool
-    })
+    return flask.jsonify({"transaction_pool": transaction_pool})
 
 
-@app.route("/add_transaction", methods=['POST'])
+@app.route("/add_transaction", methods=["POST"])
 def add_transaction():
     # transaction should contain sender public key , receiver public key and amount
     transaction = request.get_json(force=True)
-    if not transaction['sender'] or not transaction['receiver']:
-        return flask.jsonify({
-            "message": "Transaction format invalid...transaction should contain sender public key , receiver public key ."
-        })
+    if not transaction["sender"] or not transaction["receiver"]:
+        return flask.jsonify(
+            {
+                "message": "Transaction format invalid...transaction should contain sender public key , receiver public key ."
+            }
+        )
     blockchain.add_transaction(transaction)
-    return flask.jsonify({
-        "message": "transaction added successfully."
-    })
+    return flask.jsonify({"message": "transaction added successfully."})
+
 
 # validate transactions and add new blocks
 
@@ -96,22 +88,21 @@ def add_transaction():
 @app.route("/mine")
 def mine():
     if not blockchain.mine():
-        return flask.jsonify({
-            "message": "No Validators ...."
-        })
-    return flask.jsonify({
-        "message": "mining successfull."
-    })
+        return flask.jsonify({"message": "No Validators ...."})
+    return flask.jsonify({"message": "mining successfull."})
+
 
 # Todo : create a mapping table for which node is connetced to which node and later on also the public key also
+
 
 @app.route("/sendDataToPublicChain", methods=["POST"])
 def sendDataToPublicChain():
     data = request.get_json()
+    port = request.environ.get("SERVER_PORT")
+    mapping = db.Public_Private_Mapping.find_one({"Private_Address_Port": int(port)})
+
     if not data:
-        return flask.jsonify({
-            "message": "No data provided"
-        })
+        return flask.jsonify({"message": "No data provided"})
     validators = blockchain.view_all_validators()
     sender_id = data.get("sender_id")
     sender_id_found = False
@@ -122,21 +113,19 @@ def sendDataToPublicChain():
             break
 
     if not sender_id_found:
-        return flask.jsonify({
-            "message": f"sender_id '{sender_id}' is not in the list of validators"
-        })
+        return flask.jsonify(
+            {"message": f"sender_id '{sender_id}' is not in the list of validators"}
+        )
 
-    
-    url = "http://127.0.0.1:{0}/newPrivateBlockTransaction".format(mapping.get("publicChainNode"))
+    url = "{0}/newPrivateBlockTransaction".format(mapping.get("Public_Address"))
 
     json_data = json.dumps(data, default=str, sort_keys=True)
-    
+
     requests.post(url, json=json_data)
 
-    return flask.jsonify({
-        "message": "Transaction successfull."
-    })
+    return flask.jsonify({"message": "Transaction successfull."})
 
 
-if __name__ == '__main__':
-    app.run(host="0.0.0.0", port="8000", debug=True)
+if __name__ == "__main__":
+    port = int(sys.argv[1])
+    app.run(host="0.0.0.0", port=port, debug=True)
--- server.py
+++ server.py
@@ -1,22 +1,26 @@
 from hashlib import sha512
 import json
+import sys
 from bson import ObjectId
 import time
 import random
 from flask_pymongo import PyMongo
 
-from flask import Flask, request
+from flask import Flask, request, jsonify
 import requests
-
+from urllib.parse import urlparse
 
 
 # Flask web application
 # create Flask web application
 app = Flask(__name__)
 
-app.config['MONGO_URI'] = "mongodb+srv://aniket15970:3Zmg1JzbEZWO3Gjw@esm.nrjexdy.mongodb.net/ums?retryWrites=true&w=majority"
+app.config["MONGO_URI"] = (
+    "mongodb+srv://aniket15970:3Zmg1JzbEZWO3Gjw@esm.nrjexdy.mongodb.net/ums?retryWrites=true&w=majority"
+)
 mongo = PyMongo(app)
 db = mongo.db
+
 
 # A class that represents a Block, whcih stores one or more
 # pieces of data, in the immutable Blockchain
@@ -31,33 +35,43 @@ class Block:
         self.nonce = 0
         self.hash = self.compute_hash()
 
-
     # A function that creates the hash of the block content
     def compute_hash(self):
-        block_string = str(self.index)  + str(self.transactions) + str(self.previous_hash) + str(self.nonce)
+        block_string = (
+            str(self.index)
+            + str(self.transactions)
+            + str(self.previous_hash)
+            + str(self.nonce)
+        )
         # block_string = json.dumps(self.__dict__, sort_keys=True)
         return sha512(block_string.encode()).hexdigest()
+
+
 # End of Block class
+
 
 def unconfirmed_transactions():
     all_pending_txn = db.pendingTransaction.find()
     pendingTxn = []
 
     for data in all_pending_txn:
-        
-        del data['_id']
+
+        del data["_id"]
         pendingTxn.append(data)
 
     return pendingTxn
+
 
 # A class that represents an immutable list of Block objects are chained together by hashes, a Blockchain.
 class Blockchain:
     # Difficult of PoW algorithm
     difficulty = 2
+
     # One or more blocks will be stored and chained toghether on the Blockchain, starting by the genisi block
     def __init__(self):
         # self.unconfirmed_transactions = self.unconfirmed_transactions()  # These are pieces of data that are not yet added to the Blockchain.
-        self.chain = [] # The immutable list that represets the actual Blockchain
+        self.chain = []  # The immutable list that represets the actual Blockchain
+        self.peers = set()
         self.create_genesis_block()
 
     # Generates genesis block and appends it to the Blockchain
@@ -66,6 +80,7 @@ class Blockchain:
         genesis_block = Block(0, [], time.time(), "0")
         genesis_block.hash = genesis_block.compute_hash()
         self.chain.append(genesis_block)
+        self.peers.add("127.0.0.1:5000")
 
     # Verifed block can be added to the chain, add it and return True or False
     def add_block(self, block, proof):
@@ -73,13 +88,18 @@ class Blockchain:
         # Verify that the prvious_hash field of the block to be added points to the hash of the latest block
         # and tjhat the PoW that is provided is correct
         # print(self.is_valid_proof(block, proof))
-        if (previous_hash !=block.previous_hash or not self.is_valid_proof(block, proof)):
+        if previous_hash != block.previous_hash or not self.is_valid_proof(
+            block, proof
+        ):
             return False
         # Add new block to the chain after verification
         block.hash = proof
         self.chain.append(block)
         return True
 
+    def register_node(self, address):
+        parsed_url = urlparse(address)
+        self.peers.add(parsed_url.netloc)
 
     # Serve as interface to add the transactions to the blockchain by adding them
     # and then figuring out the PoW
@@ -90,10 +110,9 @@ class Blockchain:
             return False
         last_block = self.last_block
         # Creates a new block to be added to the chain
-        new_block = Block(last_block.index + 1, \
-                    pending_txns, \
-                    time.time(), \
-                    last_block.hash)
+        new_block = Block(
+            last_block.index + 1, pending_txns, time.time(), last_block.hash
+        )
 
         # Running PoW algorithm to obtain valid has and consensus
         # proof = self.proof_of_work(new_block)
@@ -108,8 +127,6 @@ class Blockchain:
         # Returns the index of the blockthat was added to the chain
         return new_block.index
 
-
-
         # proof of work algorithm that tries different values of nonce in order to get a hash
         # that satistfies the difficulty criteria
 
@@ -121,12 +138,11 @@ class Blockchain:
             computed_hash = block.compute_hash()
         return computed_hash
 
-
     # Adds a new transaction to the list of unconfirmed transactions(not yet in the blockchain)
     def add_new_transaction(self, transaction):
         res = None
         prevTxn = unconfirmed_transactions()
-        if(len(prevTxn)>=10):
+        if len(prevTxn) >= 10:
             res = mine_in_interval()
 
         db.pendingTransaction.insert_one(transaction)
@@ -145,40 +161,40 @@ class Blockchain:
             block_hash = block.hash
             # Removes the hash attributes to recompute the hash again using compute_hash
             delattr(block, "hash")
-            if not cls.is_valid_proof(block, block.hash) or previous_hash != block.previous_hash:
+            if (
+                not cls.is_valid_proof(block, block.hash)
+                or previous_hash != block.previous_hash
+            ):
                 result = False
                 break
             block.hash = block_hash
             previous_hash = block_hash
         return result
 
+        # Returns the current last Block in the Blockchain
 
-            # Returns the current last Block in the Blockchain
     @classmethod
     def is_valid_proof(cls, block, block_hash):
-        return (block_hash.startswith("0" * Blockchain.difficulty) and block_hash == block.compute_hash())
+        return (
+            block_hash.startswith("0" * Blockchain.difficulty)
+            and block_hash == block.compute_hash()
+        )
 
     @property
     def last_block(self):
         return self.chain[-1]
-# End of Blockchain class
 
+
+# End of Blockchain class
 
 
 # The node's copy of the blockchain
 blockchain = Blockchain()
-# A set that stores the addresses to other participating members in the network
-peers = set()
-peers.add("127.0.0.1:5000")
-peers.add("127.0.0.1:5001")
-peers.add("127.0.0.1:5002")
-peers.add("127.0.0.1:5003")
-peers.add("127.0.0.1:5004")
 
-# previous_item = None
 
 tempList = []
-peerList = list(peers)
+peerList = list(blockchain.peers)
+
 
 def getMinner():
     global tempList
@@ -198,62 +214,58 @@ def getMinner():
 def new_transaction():
     tx_data = request.get_json()
     print(tx_data)
-    required_fields = ["collegeName", "phoneNo", "email", "message" ]
+    required_fields = ["collegeName", "phoneNo", "email", "message"]
     for field in required_fields:
         if not tx_data.get(field):
             return "Invalid transaction data", 404
     tx_data["timestamp"] = time.time()
     res = blockchain.add_new_transaction(tx_data)
-    
-    if res==None:
+
+    if res == None:
         response = {"message": "Transaction added successfully."}
     else:
-        response = {"message" : "Transaction added successfully." , "Mine" : res}
+        response = {"message": "Transaction added successfully.", "Mine": res}
     return response, 201
-
 
 
 @app.route("/newPrivateBlockTransaction", methods=["POST"])
 # Submit a new transaction, which add new data to the blochain
 def newPrivateBlockTransaction():
-    tx_data = json.loads(request.get_json()) 
-    
-    required_fields = ["sender_id", "message" ]
+    tx_data = json.loads(request.get_json())
+
+    required_fields = ["sender_id", "message"]
     for field in required_fields:
         if not tx_data.get(field):
             return "Invalid transaction data", 404
     tx_data["timestamp"] = time.time()
     res = blockchain.add_new_transaction(tx_data)
-    
-    if res==None:
+
+    if res == None:
         response = {"message": "Transaction added successfully."}
     else:
-        response = {"message" : "Transaction added successfully." , "Mine" : res}
+        response = {"message": "Transaction added successfully.", "Mine": res}
     return response, 201
 
 
-
-
-#*image upload
-@app.route('/upload', methods=['POST'])
+# *image upload
+@app.route("/upload", methods=["POST"])
 def upload_image():
     # Check if 'image' file was included in the request
-    if 'image' not in request.files:
-        return 'No image file provided', 400
+    if "image" not in request.files:
+        return "No image file provided", 400
 
-    image_file = request.files['image']
+    image_file = request.files["image"]
 
     image_data = image_file.read()
-    tx_data = {"image" : image_data , "timestamp":time.time()}
+    tx_data = {"image": image_data, "timestamp": time.time()}
 
     res = blockchain.add_new_transaction(tx_data)
-    
-    if res==None:
+
+    if res == None:
         response = {"message": "Transaction added successfully."}
     else:
-        response = {"message" : "Transaction added successfully." , "Mine" : res}
+        response = {"message": "Transaction added successfully.", "Mine": res}
     return response, 201
-
 
 
 @app.route("/chain", methods=["GET"])
@@ -262,8 +274,8 @@ def get_chain():
     chain_data = []
     for block in blockchain.chain:
         chain_data.append(block.__dict__)
-    return {"length" : len(chain_data), "chain" : chain_data} , 200
-        
+    return {"length": len(chain_data), "chain": chain_data}, 200
+
 
 # Create a new endpoint and bind the function to the URL
 @app.route("/mine", methods=["GET"])
@@ -271,20 +283,33 @@ def get_chain():
 def mine_uncofirmed_transactions():
     result = blockchain.mine()
     if not result:
-        return {"message": "There are not transactions to mine"},200
+        return {"message": "There are not transactions to mine"}, 200
     response = "Block #{0} has been mined.".format(result)
-    return {"message" : response},200
+    return {"message": response}, 200
 
 
-# Cretes a new endpoint and binds the function to the URL
-# Adds new peers to the network
-def register_new_peers():
-    nodes = request.get_json()
-    if not nodes:
-        return "Invalid data", 400
-    for node in nodes:
-        peers.add(node)
-    return "Success", 201
+@app.route("/nodes/register", methods=["POST"])
+def register_nodes():
+    values = request.get_json()
+    Public_Address = values.get("Public_Address")
+    Private_Address_Port = values.get("Private_Address")
+
+    if (Public_Address or Private_Address_Port) is None:
+        return "Please supply a valid Private and Public Address", 400
+    blockchain.register_node(Public_Address)
+
+    payload = {
+        "Public_Address": Public_Address,
+        "Private_Address_Port": Private_Address_Port,
+    }
+
+    db.Public_Private_Mapping.insert_one(payload)
+
+    response = {
+        "message": "New node have been successfully added",
+        "total_nodes": list(blockchain.peers),
+    }
+    return jsonify(response), 201
 
 
 # Create new endpoint and bind the function to the URL
@@ -298,27 +323,7 @@ def get_pending_tx():
         del data["_id"]
         pendingTxn.append(json.loads(json.dumps(data)))
 
-    return {"data" : pendingTxn}
-
-
-# A simple algorithm to achieve consensus to mantain the intergrity of Blochain
-# If a longer valid chain is found, the chain is replaced with it and returns True, otherwise nothing happens and returns false
-#* def consensus():
-#     global blockchain
-#     longest_chain = None
-#     curr_len = len(blockchain.chain)
-#     # Achieve consensus by chacking th Json fields of every node in the network
-#     for node in peers:
-#         response = requests.get("http://{0}".format(node))
-#         length = response.json()["length"]
-#         chain = response.json()["chain"]
-#         if length > curr_len and blockchain.check_chain_validity(chain):
-#             curr_len = length
-#             longest_chain = chain
-#     if longest_chain:
-#         blockchain = longest_chain
-#         return True
-#     return False
+    return {"data": pendingTxn}
 
 
 # Create a new endpoint and binds the function to the URl
@@ -328,7 +333,12 @@ def validate_and_add_block():
     block_data = json.loads(request.get_json())
     # print((block_data) )
     # print("----------------")
-    block = Block(block_data["index"],  block_data["transactions"],  block_data["timestamp"], block_data["previous_hash"])
+    block = Block(
+        block_data["index"],
+        block_data["transactions"],
+        block_data["timestamp"],
+        block_data["previous_hash"],
+    )
     # proof = block_data["hash"]
     proof = blockchain.proof_of_work(block)
     added = blockchain.add_block(block, proof)
@@ -339,29 +349,29 @@ def validate_and_add_block():
 
 # Announce to the network once a block has been moned
 
+
 def announce_new_block(block):
-    for peer in peers:
+    for peer in blockchain.peers:
         url = "http://{0}/add_block".format(peer)
         data = block.__dict__
 
         # Convert ObjectId to string before serialization
-        data['previous_hash'] = str(data['previous_hash'])
+        data["previous_hash"] = str(data["previous_hash"])
 
         # Serialize the data to JSON
-        json_data = json.dumps(data,  default=str, sort_keys=True)
+        json_data = json.dumps(data, default=str, sort_keys=True)
 
         requests.post(url, json=json_data)
 
 
-
 def mine_in_interval():
     minner = getMinner()
-    url = f'http://{minner}/mine'
+    url = f"http://{minner}/mine"
     res = requests.get(url)
-    return res.json().get('message')
+    return res.json().get("message")
 
 
 # Run the Flask web app
 if __name__ == "__main__":
-    port = int(input("Enter the port number for this node: "))
+    port = int(sys.argv[1])
     app.run(host="0.0.0.0", port=port, debug=True)
